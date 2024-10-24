d842ba213959f981d5cb0843b98a3faa36bc2f66
mp.samaniego20 <mp.samaniego20@info.uas.edu.mx>
2024-10-23 21:49:42 -0600

    Añadir dispositivo nuevo

---
+++ funciones.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const verMasBtn = document.getElementById("verMasBtn");
+  
+    verMasBtn.addEventListener("click", function(event) {
+      event.preventDefault();
+      alert("¡Más promociones próximamente!");
+    });
+  });
+  
