2c1e238afe443fcfc87a49c1aeb6ba957c3e83b7
mp.samaniego20 <mp.samaniego20@info.uas.edu.mx>
2024-10-25 12:49:52 -0700

    menu lateral

--- funciones.js
+++ funciones.js
@@ -1,9 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const verMasBtn = document.getElementById("verMasBtn");
-  
-    verMasBtn.addEventListener("click", function(event) {
-      event.preventDefault();
-      alert("¡Más promociones próximamente!");
-    });
+  const verMasBtn = document.getElementById("verMasBtn");
+
+  verMasBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("¡Más promociones próximamente!");
   });
-  
+});
+
+  /* Función para abrir el menú lateral */
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+
+  /* Función para cerrar el menú lateral */
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
