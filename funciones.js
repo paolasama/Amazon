01200d64df21a7fcd48cf888577e5b5e1572ff37
mp.samaniego20 <mp.samaniego20@info.uas.edu.mx>
2024-10-26 16:26:34 -0700

    Corregir errores del sistema

--- funciones.js
+++ funciones.js
@@ -16,3 +16,30 @@ document.addEventListener("DOMContentLoaded", function() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+  let currentIndex = 0;
+
+    function showSlide(index) {
+      const slides = document.getElementsByClassName('carousel-item');
+      if (index >= slides.length) {
+        currentIndex = 0;
+      } else if (index < 0) {
+        currentIndex = slides.length - 1;
+      } else {
+        currentIndex = index;
+      }
+      const offset = -currentIndex * 100;
+      document.getElementById('carouselInner').style.transform = 'translateX(' + offset + '%)';
+    }
+
+    function nextSlide() {
+      showSlide(currentIndex + 1);
+    }
+
+    function prevSlide() {
+      showSlide(currentIndex - 1);
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+      showSlide(currentIndex);
+    });
