d51116b98eb780102c65842f5ce4f12be3330235
mp.samaniego20 <mp.samaniego20@info.uas.edu.mx>
2024-11-16 19:23:50 -0700

    unit tests

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,24 +1,38 @@
 <?php
-
+ 
 namespace App\Providers;
-
+ 
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\ParallelTesting;
 use Illuminate\Support\ServiceProvider;
-
+use PHPUnit\Framework\TestCase;
+ 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        ParallelTesting::setUpProcess(function (int $token) {
+            // ...
+        });
+ 
+        ParallelTesting::setUpTestCase(function (int $token, TestCase $testCase) {
+            // ...
+        });
+ 
+        // Executed when a test database is created...
+        ParallelTesting::setUpTestDatabase(function (string $database, int $token) {
+            Artisan::call('db:seed');
+        });
+ 
+        ParallelTesting::tearDownTestCase(function (int $token, TestCase $testCase) {
+            // ...
+        });
+ 
+        ParallelTesting::tearDownProcess(function (int $token) {
+            // ...
+        });
     }
 }
