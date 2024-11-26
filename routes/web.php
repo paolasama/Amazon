206b0297b0e7d10e152e0227d4589f733d917fb7
mp.samaniego20 <mp.samaniego20@info.uas.edu.mx>
2024-11-25 21:26:07 -0700

    Descripción de los cambios realizados en web.php

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Homecontroller;
+use App\Http\Controllers\SearchController;
 
 Route::get('/', [Homecontroller::class, 'index']);
+
+Route::get('/search', [SearchController::class, 'index'])->name('search');
+
